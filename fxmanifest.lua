855e18a85137dfdeaf843989a1aceb0ec72a3c53
VT Developmet's <banowski0810@gmail.com>
2022-09-15 14:56:14 +0300

    update
    js update and minor css changes

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+
+game 'gta5'
+author "Snipe edit style VT Developmtnet's"
+description 'Thermite Game Like NoPixel'
+version '1.0.1'
+server_scripts {
+  --'server/server.lua',
+}
+
+client_scripts {
+  'client/main.lua',
+}
+
+ui_page {
+  'html/index.html'
+}
+
+files {
+  'html/index.html',
+  'html/style.css',
+  'html/app.js'
+}
